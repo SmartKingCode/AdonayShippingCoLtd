0760657b0c24662c5bff8519038da0accb404f52
SmartKingCode <45896189+SmartKingCode@users.noreply.github.com>
2019-02-19 10:06:22 +0000

    Caption

--- ASC/public_html/src/scripts/messages.js
+++ ASC/public_html/src/scripts/messages.js
@@ -9,9 +9,16 @@ const messages = {
         service: 'Services',
         about: 'About Us',
         contact: 'Contact Us',
+        read: 'Read more',
         caption11:'is a company of ,',
         caption12: 'consignment in the maritime sector .',
-        caption13: 'It is ranked among the best in TOGO'
+        caption13: 'It is ranked among the best in TOGO',
+        caption21: 'offers of services,',
+        caption22: 'refueling in water and provisions',
+        caption23: 'ships in the port and in the port of Lome',
+        caption31: 'supports ,',
+        caption32: 'the preparation and start-up of seafarers',
+        caption33: 'who embarks on board foreign ships'
        
     },
     'fr': {
@@ -19,9 +26,16 @@ const messages = {
        service: 'Services',
        about: 'A propos',
        contact: 'Contactez',
+       read: 'Lire plus',
        caption11:'est une entreprise de , ',
        caption12: 'consignation dans le secteur maritime .',
-       caption13: 'Elle est classée parmi les meilleurs au TOGO'
+       caption13: 'Elle est classée parmi les meilleurs au TOGO',
+       caption21: 'offres des services,',
+       caption22: `d'avitaillement en eaux et provisions`,
+       caption23: ' des navires dans le port et en rade au Port Autonome de Lomé',
+       caption31: 'prend en charge ,',
+       caption32: ' la préparation  et mise en route des marins',
+       caption33: 'qui embarque à bord des navires étrangers'
         
     }
 };
