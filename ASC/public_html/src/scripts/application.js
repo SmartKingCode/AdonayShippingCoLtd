b76a4acc8ac3a206d3d8fa673be54b4e7ae27816
SmartKingCode <45896189+SmartKingCode@users.noreply.github.com>
2019-02-28 22:15:11 +0000

    Responsive Layout

--- ASC/public_html/src/scripts/application.js
+++ ASC/public_html/src/scripts/application.js
@@ -7,13 +7,17 @@
 
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'animate.css/animate.min.css';
 import '../stylesheets/styles.scss';
 
 import '@fortawesome/fontawesome-free/js/all';
 
 import Vue from 'vue';
+import loadMap from 'load-google-maps-api';
 
 import i18n from './messages';
+
+
 
 
 
@@ -35,6 +39,20 @@ new Vue({
     }
     
 }).$mount('#container');
+
+
+
+    loadMap().then( function(googleMaps){
+      new google.maps.Map(document.getElementById('map'), {
+     
+        center: {lat: -25.344, lng: 131.036},
+        zoom: 4,
+        key: "AIzaSyAH780HlPY6aFgaHlPgZ_9CD6XOnumZN8o"
+      })
+    }).catch(function(error){
+      console.error(error)
+    })
+
 
 
 
